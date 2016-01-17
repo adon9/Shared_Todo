8fe3114893fe203407c7f14750bd028b7fe837e5
hebaab <its.mylife-@live.com>
2016-01-17 17:39:19 -0600

    Initial Commit

---
+++ Shared_Todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shared_Todo
+//
+//  Created by Jayda'a Alidrisi on 1/17/16.
+//  Copyright © 2016 GigiSky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
